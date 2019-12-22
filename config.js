833ffc3cebe7abe73776c44ed299d66b11d5c3dc
Glitch (salllllaaa) <none>
2019-12-22 07:51:48 +0000

    👽🌾 Checkpoint ./config.json:9471043/236 ./config.js:9471043/236

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "507803933557915652", //kendi IDınızı yazınız
     "admins": ["336869318874890241"],
     "support": ["336869318874890241"],
-    "token": "NTY0MDkzNDIzNzU0MjgwOTYw.XKi2-A.8NonFn5TRtv_I0IXsS5IBGOeqsM", //botunuzun tokenini yazınız
+    "token": "NjU4MjAyMzI3NzIzMDE2MjEy.Xf8e9Q.Z5mawGqeYKkkQQIKvC9wXyyTkiY", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "lQRQ4QWq0VXuzlFyaTSidt5qr1xc2H8X", //botunuzun secretini yazınız
       "callbackURL": `http://panel.leinxs.xyz/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
