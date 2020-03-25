e6a4bc0f75227f172c31b3fadac57a65c6ccec6c
Glitch (botmee6) <none>
2020-03-25 11:32:34 +0000

    🚙📍 Checkpoint ./config.js:5204686/238 ./panel/html/anasayfa.ejs:5204686/846 ./panel/html/leveling.ejs:5204686/76 ./panel/html/moderation.ejs:5204686/76 ./panel/html/music.ejs:5204686/76

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
     "ownerID": "493675583742672896", //kendi IDınızı yazınız
-    "admins": ["id", "id2"],
+    "admins": ["482910932725661696", "690929443719610418"],
     "token": "NjkyMzA5MzQ2MjY4NDc5NTI4.XnsqLQ.ol4wxGBnKxsRoQM1RrBdvCxRnu8", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "oauthsecret", //botunuzun secretini yazınız
-      "callbackURL": `https://sitelink.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "oauthSecret": "dhzFpK_zT0rSbMHBqs3Iof4qXTJFObN4", //botunuzun secretini yazınız
+      "callbackURL": `https://berry1.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "xyzxyz", //kalsın
-      "domain": "https://sitelink.glitch.me" //site URLnizi yazınız!
+      "domain": "https://berry1.glitch.me" //site URLnizi yazınız!
     }
   };
   
